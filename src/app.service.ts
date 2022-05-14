3c24364dac17e13e2ca258f6a6dbc083b047a25f
quanglinh2909 <hackcoquanglinh2000@gmail.com>
2022-05-14 15:51:44 +0700

    [dev-linh] hello 1

--- src/app.service.ts
+++ src/app.service.ts
@@ -4,7 +4,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    //ngan hello
+    //ngan hello 1
     return 'Hello World!';
   }
 }
